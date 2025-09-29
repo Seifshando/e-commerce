1d8b8006a4671600c3b77f93050fc08782a1e13b
Seif Eldin <seifshando244@gmail.com>
2025-09-29 13:44:22 +0300

    Update getMyToken.ts
    handle getMyToken

--- src/app/getMyToken/getMyToken.ts
+++ src/app/getMyToken/getMyToken.ts
@@ -4,13 +4,17 @@ import { cookies } from "next/headers";
 
 
 export default async function getMyToken(){
-  const decodedToken = (await cookies()).get("next-auth.session-token")?.value;
+  const decodedToken = 
+    (await cookies()).get("next-auth.session-token")?.value || 
+    (await cookies()).get("__Secure-next-auth.session-token")?.value ;
+
+  if(!decodedToken) return null;
 
   const token = await decode({token: decodedToken, secret: process.env.NEXTAUTH_SECRET!});
 
 
 
-  return token?.token;
+  return token?.token || null;
 }
 
 
